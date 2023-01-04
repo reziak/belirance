3eeecb58f54bfc8beb1063ac62140677331fa84b
Bruno Lira <bruno.padovan@gmail.com>
2023-01-04 20:57:55 -0300

    add cors

--- backend/src/app/server.ts
+++ backend/src/app/server.ts
@@ -1,11 +1,13 @@
 import 'express-async-errors';
 import express from 'express';
 import helmet from 'helmet';
+import cors from 'cors';
 import { routes } from './routes';
 import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 
+app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use('/api', routes);
